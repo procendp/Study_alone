35af56920c97b870c858ab19078f9067bbb012fd
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-12-02 22:33:18 +0900

    DBN basic

---
+++ 이것이_코딩테스트다_동빈나/4_조건문.py
@@ -0,0 +1,14 @@
+# 조건문
+
+# pass 키워드
+# 조건문 안을 지금 당장 처리할 것이 없을 때 사용
+a = 5
+if a > 7 :
+    pass
+else :
+    print(a)
+
+# 조건문 한 줄로 표현하기
+score = 85
+
+result = "참" if score >= 80 else "거짓" # if 앞이 참, else 뒤가 거짓
---
+++ 이것이_코딩테스트다_동빈나/7_표준 라이브러리.py
@@ -0,0 +1,19 @@
+# 내장 함수
+    # print(), input(), sorted() ...
+# itertools
+    # 순열과 조합 제공
+# heapq
+    # 힙 기능 제공, 그래프 알고리즘 때 사용할 것
+# bisect
+    # 이진탐색(binary search) 기능을 제공, 특정 리스트가 정렬되어 있을 때, 그 리스트에서 어떤 값들을 빠르게 찾고자할 때 사용, 코테에서 이진탐색 문제에서 잘 쓰임
+# collections
+    # 덱(deque)이나 카운터(counter) 같이 유용한 자료구조 알고리즘에 사용
+
+
+
+# sum()
+# min(), max()
+
+# eval() - 중위표기법 시 사용
+result = eval("(3+7+4)*3")
+print(result)
